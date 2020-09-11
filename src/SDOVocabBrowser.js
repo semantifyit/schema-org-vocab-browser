3c4ca61dc46ec6c42730bdacd47f2bcd8bc0bf5d
YarnSeemannsgarn <janschlenker1990@gmail.com>
2020-09-11 11:20:14 +0200

    Add function to generate content section

--- src/SDOVocabBrowser.js
+++ src/SDOVocabBrowser.js
@@ -8,8 +8,18 @@ class SDOVocabBrowser {
             this.sdoAdapter = new SDOAdapter();
 
             // TODO: differentiate between vocab and list
+            // TODO: Add loader
             const sdoURL = await this.sdoAdapter.constructSDOVocabularyURL('latest', 'all-layers');
             await this.sdoAdapter.addVocabularies([sdoURL, vocabOrVocabList]);
+
+            let vocabs = this.sdoAdapter.getVocabularies(this.vocabOrVocabList);
+            delete vocabs['schema'];
+            const vocabNames = Object.keys(vocabs);
+            this.classes = this.sdoAdapter.getListOfClasses({ fromVocabulary: vocabNames});
+            this.properties = this.sdoAdapter.getListOfProperties({fromVocabulary: vocabNames});
+            this.enumerations = this.sdoAdapter.getListOfEnumerations({fromVocabulary: vocabNames});
+            this.enumerationMembers = this.sdoAdapter.getListOfEnumerationMembers({fromVocabulary: vocabNames});
+            this.dataTypes = this.sdoAdapter.getListOfDataTypes({fromVocabulary: vocabNames});
 
             this.generateHTML();
 
@@ -18,21 +28,39 @@ class SDOVocabBrowser {
     }
 
     generateHTML() {
+        let html = this.generateContentSection();
+
+        // TODO: Add other html
+
+        this.elem.innerHTML = html;
+    }
+
+    generateContentSection() {
         let html = '<b>Content</b>' +
             '<ul>';
 
-        let vocabs = this.sdoAdapter.getVocabularies(this.vocabOrVocabList);
-        delete vocabs['schema'];
-        const vocabNames = Object.keys(vocabs);
-        const classes = this.sdoAdapter.getListOfClasses({ fromVocabulary: vocabNames });
-        // TODO: Add other stuff
+        if (this.classes.length !== 0) {
+            html += '<li>' + this.classes.length + ' Classes</li>';
+        }
 
-        html += '<li>' + classes.length + ' Classes</li>';
+        if (this.properties.length !== 0) {
+            html += '<li>' + this.properties.length + ' Properties</li>';
+        }
+
+        if (this.enumerations.length !== 0) {
+            html += '<li>' + this.enumerations.length + ' Enumerations</li>';
+        }
+
+        if (this.enumerationMembers.length !== 0) {
+            html += '<li>' + this.enumerationMembers.length + ' Enumeration Members</li>';
+        }
+
+        if (this.dataTypes.length !== 0) {
+            html += '<li>' + dataTypes.length + ' Data Types</li>';
+        }
+
         html += '</ul>';
-
-        // TODO: Add more
-
-        this.elem.innerHTML = html;
+        return html;
     }
 }
 
